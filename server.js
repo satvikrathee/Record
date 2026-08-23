151639fbf953566d13c6328de7c2f8545f3002bb
Satvik Rathee <satvikrathee@gmail.com>
2026-08-23 21:20:51 +0530

    nameechange

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://satvikrecord.vercel.app', 'http://localhost:5173'],
+  origin: ['https://satvikdairyrecord.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
